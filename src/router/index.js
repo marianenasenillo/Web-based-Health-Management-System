273622d7c7b22f66fc326b202db5bdc03350c2c8
Leovin <Leovinjozhsalarda@gmail.com>
2025-09-30 20:30:28 +0800

    adding childcare

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import DashboardView from '@/components/DashboardView.vue'
 import HomeView from '@/views/HomeView.vue'
 import HpsView from '@/views/HpsView.vue'
 import Maternal from '@/views/Maternal.vue'
+import Childcare from '@/views/Childcare.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,11 @@ const router = createRouter({
       path: '/maternalservices',
       name: 'maternalservices',
       component: Maternal,
+    },
+    {
+      path: '/childcare',
+      name: 'childcare',
+      component: Childcare,
     },
   ],
 })
