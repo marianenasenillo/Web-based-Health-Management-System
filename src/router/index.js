9043de1cc9e2afd38c4d33234fe6963602a6229e
Leovin <Leovinjozhsalarda@gmail.com>
2025-09-30 22:42:32 +0800

    adding calendar but not finnish yet

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Maternal from '@/views/Maternal.vue'
 import Childcare from '@/views/Childcare.vue'
 import Familyplanning from '@/views/Familyplanning.vue'
 import Phs from '@/views/Phs.vue'
+import Calendar from '@/views/Calendar.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -50,6 +51,11 @@ const router = createRouter({
       path: '/preventivehealhservices',
       name: 'preventivehealhservices',
       component: Phs,
+    },
+    {
+      path: '/calendar',
+      name: 'calendar',
+      component: Calendar,
     },
   ],
 })
